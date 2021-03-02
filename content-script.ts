b1e282a6deb83cdd8ff734ba4db432ad96bafd42
LFN <liangfengning@foxmail.com>
2021-03-02 18:03:53 +0800

    fix: use dynamic import to reduce useless insertions

--- content-script.ts
+++ content-script.ts
@@ -1,13 +1,32 @@
-import applyOption from './components/applyOption'
+// import applyOption from './components/applyOption'
 import checkPage from './components/checkPage'
-import generateClickableURL from './components/generateClickableURL'
-import insertEditor from './components/insertEditor'
-import insertToolBar from './components/insertToolBar'
+// import generateClickableURL from './components/generateClickableURL'
+// import insertEditor from './components/insertEditor'
+// import insertToolBar from './components/insertToolBar'
 
-checkPage().then(content => {
+checkPage().then(async content => {
   if (content) {
-    const code = insertEditor(content)
-    insertToolBar(code, content)
+    const insertEditor = await import('./components/insertEditor')
+    const insertToolBar = await import('./components/insertToolBar')
+
+    const applyOptionDelay = (code: CodeMirror.Editor) => {
+      import('./components/applyOption').then(module => {
+        setTimeout(() => {
+          module.default(code)
+        }, 0)
+      })
+    }
+
+    const generateClickableURLDelay = () => {
+      import('./components/generateClickableURL').then(module => {
+        setTimeout(() => {
+          module.default()
+        }, 300)
+      })
+    }
+
+    const code = insertEditor.default(content)
+    insertToolBar.default(code, content)
     applyOptionDelay(code)
     generateClickableURLDelay()
     code.on('unfold', () => {
@@ -15,15 +34,3 @@ checkPage().then(content => {
     })
   }
 })
-
-const applyOptionDelay = (code: CodeMirror.Editor) => {
-  setTimeout(() => {
-    applyOption(code)
-  }, 0)
-}
-
-const generateClickableURLDelay = () => {
-  setTimeout(() => {
-    generateClickableURL()
-  }, 300)
-}
